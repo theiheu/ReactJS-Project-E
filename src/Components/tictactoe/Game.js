11900d4877d87d92713b999e64bae92fdad4267f
Thế Hiếu <thehieu236@gmail.com>
2022-12-28 16:41:44 +0700

    update tictactoe

---
+++ src/Components/tictactoe/Game.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Board from "./Board";
+import "./GameStyle.css";
+
+const Game = () => {
+  return (
+    <div>
+      <Board></Board>
+    </div>
+  );
+};
+
+export default Game;
